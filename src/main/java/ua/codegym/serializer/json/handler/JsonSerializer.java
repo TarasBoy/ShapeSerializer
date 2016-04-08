a7b9a3e12a913327bf254a58c21157137be36539
tboichenko <taras.b@webgroup-limited.com>
2016-04-08 18:33:17 +0300

    add group for json

--- src/main/java/ua/codegym/serializer/json/handler/JsonSerializer.java
+++ src/main/java/ua/codegym/serializer/json/handler/JsonSerializer.java
@@ -1,18 +1,20 @@
 package ua.codegym.serializer.json.handler;
 
-import ua.codegym.serializer.Serializer;
-import ua.codegym.serializer.shape.Shape;
-import ua.codegym.serializer.shape.Triangle;
+    import ua.codegym.serializer.Serializer;
+    import ua.codegym.serializer.shape.Group;
+    import ua.codegym.serializer.shape.Shape;
+    import ua.codegym.serializer.shape.Square;
+    import ua.codegym.serializer.shape.Triangle;
 
-import java.io.IOException;
-import java.io.OutputStream;
+    import java.io.IOException;
+    import java.io.OutputStream;
 
 public class JsonSerializer implements Serializer {
   @Override
   public void serialize(Shape shape, OutputStream os) throws IOException {
     String type = shape.getType();
-    if(type.equals(Triangle.class.getCanonicalName())){
-      Triangle triangle = (Triangle)shape;
+    if (type.equals(Triangle.class.getCanonicalName())) {
+      Triangle triangle = (Triangle) shape;
       int xA = triangle.getxA();
       int yA = triangle.getyA();
       int xB = triangle.getxB();
@@ -20,10 +22,32 @@ public class JsonSerializer implements Serializer {
       int xC = triangle.getxC();
       int yC = triangle.getyC();
 
-      String out = String.format("{\"xA\":%d,\"yA\":%d,\"xB\":%d,\"yB\":%d,\"xC\":%d,\"yC\":%d,}", xA, yA,xB,yB,xC,yC);
+      String out = String.format("{\"Triangle\":{\"xA\":%d,\"yA\":%d,\"xB\":%d,\"yB\":%d,\"xC\":%d,\"yC\":%d},", xA, yA, xB, yB, xC, yC);
 
       os.write(out.getBytes());
 
+    }
+    if (type.equals(Square.class.getCanonicalName())) {
+      Square square = (Square) shape;
+      int x = square.getX();
+      int y = square.getY();
+      int side = square.getSide();
+
+      String out = String.format("{\"Square\":{\"x\":%d,\"y\":%d,\"side\":%d},", x, y, side);
+
+      os.write(out.getBytes());
+
+    }
+
+    if (type.equals(Group.class.getCanonicalName())) {
+      Group group = (Group) shape;
+
+
+      os.write("{\"group\":[".getBytes());
+      for (Shape innerShape : group.getShapes()) {
+        serialize(innerShape, os);
+      }
+      os.write("]}".getBytes());
     }
   }
 }
--- src/test/java/ua/codegym/serializer/JsonSerializerTest.java
+++ src/test/java/ua/codegym/serializer/JsonSerializerTest.java
@@ -1,8 +1,10 @@
 package ua.codegym.serializer;
 
 import org.junit.Test;
+import ua.codegym.serializer.shape.Group;
 import ua.codegym.serializer.shape.Shape;
 import ua.codegym.serializer.shape.Triangle;
+import ua.codegym.serializer.shape.Square;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -13,7 +15,7 @@ public class JsonSerializerTest {
 
   @Test
 
-  public void serializeSingleTriangleToJson() throws IOException {
+  public void verifyThatSingleTriangleIsWritingCorrectlyToJson() throws IOException {
     Shape shape = new Triangle(1,2,3,4,5,6);
 
     ByteArrayOutputStream out = new ByteArrayOutputStream();
@@ -24,7 +26,43 @@ public class JsonSerializerTest {
 
     // then
     String output = new String(out.toByteArray());
-    assertEquals("{\"xA\":1,\"yA\":2,\"xB\":3,\"yB\":4,\"xC\":5,\"yC\":6,}", output);
+    assertEquals("{\"Triangle\":{\"xA\":1,\"yA\":2,\"xB\":3,\"yB\":4,\"xC\":5,\"yC\":6},", output);
+
+
+  }
+  @Test
+
+  public void verifyThatSingleSquareIsWritingCorrectlyToJson() throws IOException {
+    Shape shape = new Square(1,2,3);
+
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    Serializer json = SerializerFactory.newJsonSerializer();
+
+    // when
+    json.serialize(shape, out);
+
+    // then
+    String output = new String(out.toByteArray());
+    assertEquals("{\"Square\":{\"x\":1,\"y\":2,\"side\":3},", output);
+
+
+  }
+  @Test
+
+  public void verifyThatGroupIsWritingCorrectlyToJson() throws IOException {
+    Group shape = new Group();
+    shape.add(new Triangle(1,2,3,4,5,6));
+    shape.add(new Square(1,2,3));
+
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    Serializer json = SerializerFactory.newJsonSerializer();
+
+    // when
+    json.serialize(shape, out);
+
+    // then
+    String output = new String(out.toByteArray());
+    assertEquals("{\"group\":[{\"Triangle\":{\"xA\":1,\"yA\":2,\"xB\":3,\"yB\":4,\"xC\":5,\"yC\":6},{\"Square\":{\"x1\":1,\"y\":2,\"side\":3},]", output);
 
 
   }
